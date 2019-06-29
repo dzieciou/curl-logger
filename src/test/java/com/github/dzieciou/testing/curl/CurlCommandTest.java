c536e5377e0820d4f340c600ae2e0a9c88e01bf4
Maciej Gawinecki <mgawinecki@gmail.com>
2019-06-29 09:50:22 +0200

    #25 Fix escaping characters for POSIX platforms.

--- src/test/java/com/github/dzieciou/testing/curl/CurlCommandTest.java
+++ src/test/java/com/github/dzieciou/testing/curl/CurlCommandTest.java
@@ -3,6 +3,7 @@ package com.github.dzieciou.testing.curl;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
 
 import org.testng.annotations.Test;
@@ -16,11 +17,55 @@ public class CurlCommandTest {
         .addHeader("Host", "server.com")
         .addHeader("Other", "other");
 
-    assertThat(curl.asString(Platform.WINDOWS, true, true),
+    assertThat(curl.asString(Platform.WINDOWS, true, true, true),
         containsString("^"));
 
-    assertThat(curl.asString(Platform.UNIX, true, true),
+    assertThat(curl.asString(Platform.UNIX, true, true, true),
         not(containsString("^")));
   }
 
+  @Test
+  public void shouldEscapeNonAsciiCharacters() {
+
+    CurlCommand curl = new CurlCommand()
+        .setUrl("/requestPath")
+        .addDataBinary("name=Administração");
+
+    assertThat(curl.asString(Platform.UNIX, true, false, true),
+        equalTo("curl '/requestPath' --data-binary $'name=Administra\\xe7\\xe3o'"));
+  }
+
+  @Test
+  public void shouldNotEscapeNonAsciiCharacters() {
+
+    CurlCommand curl = new CurlCommand()
+        .setUrl("/requestPath")
+        .addDataBinary("name=Administração");
+
+    assertThat(curl.asString(Platform.UNIX, true, false, false),
+        equalTo("curl '/requestPath' --data-binary 'name=Administração'"));
+  }
+
+  @Test
+  public void shouldEscapeSingleQuotes() {
+
+    CurlCommand curl = new CurlCommand()
+        .setUrl("/requestPath")
+        .addDataBinary("{'name':'John'}");
+
+    assertThat(curl.asString(Platform.UNIX, true, false, true),
+        equalTo("curl '/requestPath' --data-binary $'{\\'name\\':\\'John\\'}'"));
+  }
+
+
+  @Test
+  public void shouldEscapeAtCharacter() {
+
+    CurlCommand curl = new CurlCommand()
+        .setUrl("/requestPath")
+        .addDataBinary("maciek@gmail.com");
+
+    assertThat(curl.asString(Platform.UNIX, true, false, true),
+        equalTo("curl '/requestPath' --data-binary $'maciek\\x40gmail.com'"));
+  }
 }
