5f940359e09c06f577b45902b0c78e8f33ae21db
Srepfler Srdan <schrepfler@gmail.com>
2020-08-28 14:57:32 +0200

    Update CurlCommand.java
    I hope this should fix https://github.com/dzieciou/curl-logger/issues/47 
    I can't validate as I'm not seeing any curl logging anyways but I'm not seeing the exception anymore.

--- src/main/java/com/github/dzieciou/testing/curl/CurlCommand.java
+++ src/main/java/com/github/dzieciou/testing/curl/CurlCommand.java
@@ -295,13 +295,7 @@ public class CurlCommand {
     }
 
     private static String escapeAsHex(char c) {
-      int code = (int) c;
-      String codeAsHex = Integer.toHexString(code);
-      if (code < 256) {
-        // Add leading zero when needed to not care about the next character.
-        return code < 16 ? "\\x0" + codeAsHex : "\\x" + codeAsHex;
-      }
-      return "\\u" + ("" + codeAsHex).substring(codeAsHex.length(), 4);
+      return String.format("%04x", (int) c);
     }
 
     public String serialize(CurlCommand curl) {
